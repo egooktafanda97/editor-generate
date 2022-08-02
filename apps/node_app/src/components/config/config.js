8bc5bcbd3a0ff31bd459497d29096f544023c587
egooktafanda97 <egooktafanda1097@gmail.com>
2022-08-02 16:45:50 +0700

    push baru 1 02 08 2022

--- apps/node_app/src/components/config/config.js
+++ apps/node_app/src/components/config/config.js
@@ -3,4 +3,4 @@ export const url_assets = "http://localhost/wizard-api/public/";
 export const url_base = "http://localhost/wizard-api/";
 
 // public server
-export const url_api_server = "http://v3.phoenixkreatifdigital.com/rest/api/";
+export const url_api_server = "https://v3.phoenixkreatifdigital.com/rest/api/";
